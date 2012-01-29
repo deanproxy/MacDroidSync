da8e639df7c7bc31e0bc9480785542b66111f41c
dean <dean@deanproxy.com>
2012-01-29 01:05:59 -0500

    trying to get this wxpython stuff figured out. also refactored a lot of the code

--- itunes.py
+++ itunes.py
@@ -6,70 +6,23 @@ import os
 import sys
 import urllib2
 import urlparse
-import logging
 
-from shutil import copy, Error
 from Foundation import *
 from ScriptingBridge import *
 
-import wx
 
-logging.basicConfig(filename='/tmp/syncItunes.log', level=logging.DEBUG)
+class iTunesTrack(object):
 
-kind_blacklist = ['Protected AAC audio file']
+    def __init__(self, track):
+        """ represents the essential information for a track """
 
-class ITunes(object):
-    def __init__(self, destination):
-        """ opens the iTunes library """
+        self.name = track.name()
+        self.kind = track.kind()
+        self.path = self._get_track_location(track)
 
-        self.destination = destination
-        self.itunes = SBApplication.applicationWithBundleIdentifier_("com.apple.iTunes")
-
-    def sync(self):
-        """" Copies all of the tracks to the destination """
-
-        track_list = self.itunes.sources()[0].userPlaylists()[0].fileTracks()
-        accepted_tracks = [i for i in track_list if i.kind() not in kind_blacklist]
-        total_tracks = len(accepted_tracks)
-
-        logging.info("Syncing a total of {0} tracks. {1} were not able to be synced.".
-                format(total_tracks, (len(track_list) - total_tracks)))
-
-        progress = wx.ProgressDialog('Syncing iTunes Tracks', 'Copying track...', 
-                maximum=total_tracks, style=0|wx.PD_CAN_ABORT)
-        for count,track in enumerate(accepted_tracks):
-            (keep_going,skip) = progress.Update(count, 'Copying Track: %s' % track.name().encode('utf-8'))
-            if not keep_going:
-                break
-            logging.debug('Syncing {0}'.format(track.name().encode('utf-8')))
-            self._copy_track(track)
-
-        progress.Update(total_tracks, 'Creating playlists...')
-        self._create_playlists()
-        progress.Destroy()
-
-    def _create_playlists(self):
-        """ Create all playlists in m3u format. Copy music as we go... """
-
-        real_playlist_kind = 1800302446
-        playlists = self.itunes.sources()[0].userPlaylists()
-
-        # The first playlist should be the Music playlist.
-        for playlist in playlists:
-            if playlist.specialKind() == real_playlist_kind:
-                playlist_file_name = "{0}.m3u".format(os.path.join(self.destination, playlist.name().encode('utf-8')))
-                playlist_file = open(playlist_file_name, 'w')
-                for track in playlist.fileTracks():
-                    if track.kind() not in kind_blacklist:
-                        track_path = self._relative_track_path(track)
-                        playlist_file.write(track_path.encode('utf-8'))
-                        playlist_file.write("\n")
-                playlist_file.close()
-
-    def _tracks_match(self, local, remote):
-        """ md5 takes too long, so we will just check to see if the file sizes are identical """
-
-        return os.path.getsize(local) == os.path.getsize(remote)
+    def is_protected(self):
+        kind_blacklist = ['Protected AAC audio file']
+        return self.kind in kind_blacklist
 
     def _get_track_location(self, track):
         """ Returns the absolute path to the iTunes track """
@@ -77,23 +30,38 @@ class ITunes(object):
         location = urllib2.unquote(str(track.location()))
         return urlparse.urlparse(location)[2]
 
-    def _relative_track_path(self, track):
-        """ Relative path for a track """
 
-        track_file_name = os.path.basename(get_track_location(track))
-        artist = track.artist() or 'Unknown Artist'
-        album = track.album() or 'Unknown Album'
-        return os.path.join(artist, album, track_file_name)
+class iTunesPlaylist(object):
+    def __init__(self, playlist):
+        """ represents the essential information for a playlist """
 
-    def _copy_track(self, track):
-        """ Makes sure to only copy tracks that need copying """
+        self.name = playlist.name()
+        self.kind = playlist.specialKind()
+        self.tracks = [iTunesTrack(i) for i in playlist.fileTracks()]
 
-        src_track_file = self._get_track_location(track)
-        dst_track_file = os.path.join(self.destination, self._relative_track_path(track))
-        
-        if os.path.exists(src_track_file):
-            if not os.path.exists(dst_track_file) or not self._tracks_match(src_track_file, dst_track_file):
-                if not os.path.exists(os.path.dirname(dst_track_file)):
-                    os.makedirs(os.path.dirname(dst_track_file))
-                copy(src_track_file, dst_track_file)
-        
+    def tracks_in_playlist(self, playlist):
+        """ Returns an array of iTunesTrack """
+
+        return self.tracks
+
+    def is_music_playlist(self):
+        real_playlist_kind = 1800302446
+        return self.kind == real_playlist_kind
+
+class iTunes(object):
+    def __init__(self):
+        """ opens the iTunes library """
+
+        self.itunes = SBApplication.applicationWithBundleIdentifier_("com.apple.iTunes")
+
+    def playlists(self):
+        """ Returns all of the playlists available as an array of iTunesPlaylist """
+
+        return [iTunesPlaylist(p) for p in self.itunes.sources()[0].userPlaylists()]
+
+    def tracks(self):
+        """ returns all tracks from the primary playlist (Music playlist) 
+            which should be every track available as an array of iTunesTrack """
+
+        return [iTunesTrack(t) for t in self.itunes.sources()[0].userPlaylists()[0].fileTracks()]
+
--- main.py
+++ main.py
@@ -3,13 +3,102 @@
 #from itunes import ITunes
 
 import os
+import logging 
+
+from shutil import copy, Error
+from itunes import iTunes, iTunesTrack, iTunesPlaylist
+
 import wx
+import wx.lib.filebrowsebutton as filebrowse
+import wx.lib.anchors as anchors
+
+logging.basicConfig(filename='/tmp/syncItunes.log', level=logging.DEBUG)
+
+destination = None
+
+def sync_tracks(tracks, progress=None):
+    """ 
+        Copies all of the tracks to the destination.
+        Returns total tracks copied, total tracks skipped
+    """
+
+    count = 0
+    for count,track in enumerate(tracks):
+        if progress:
+            (keep_going,skip) = progress.Update(count, 'Copying Track: %s' % track.name.encode('utf-8'))
+            if not keep_going:
+                return (count + 1, -1)
+        logging.debug('Syncing {0}'.format(track.name.encode('utf-8')))
+        copy_track(track)
+
+    return (count + 1, len(tracks) - (count+1))
+
+def sync_playlist(playlist, progress=None):
+    """ 
+        Create all playlists in m3u format. Copy music as we go... 
+        Returns total tracks copied, total tracks skipped
+    """
+
+    total_tracks = len(playlist.tracks)
+    count = 0
+
+    playlist_dir = os.path.join(destination, 'Playlists')
+    if not os.path.exists(playlist_dir):
+        os.makedirs(playlist_dir)
+
+    playlist_file_name = "{0}.m3u".format(os.path.join(playlist_dir, playlist.name.encode('utf-8')))
+    playlist_file = open(playlist_file_name, 'w')
+
+    for track in playlist.tracks:
+        if not track.is_protected:
+            (keep_going,skip) = progress.Update(count, 'Copying Track: %s' % track.name.encode('utf-8'))
+            if not keep_going:
+                return
+            copy_track(track)
+            track_path = relative_track_path(track)
+            playlist_file.write(os.path.join('..', track_path.encode('utf-8')))
+            playlist_file.write("\n")
+            count = count + 1
+
+    playlist_file.close()
+
+    return (count, total_tracks - count)
+
+
+def relative_track_path(track):
+    """ Relative path for a track """
+
+    track_file_name = os.path.basename(track.path)
+    artist = track.albumArtist() or 'Unknown Artist'
+    album = track.album() or 'Unknown Album'
+    return os.path.join(artist, album, track_file_name)
+
+def tracks_match(local, remote):
+    """ md5 takes too long, so we will just check to see if the file sizes are identical """
+
+    return os.path.getsize(local) == os.path.getsize(remote)
+
+def copy_track(track):
+    """ Makes sure to only copy tracks that need copying """
+
+    src_track_file = track.path
+    dst_track_file = os.path.join(destination, relative_track_path(track))
+    
+    if os.path.exists(src_track_file):
+        if not os.path.exists(dst_track_file) or not tracks_match(src_track_file, dst_track_file):
+            if not os.path.exists(os.path.dirname(dst_track_file)):
+                os.makedirs(os.path.dirname(dst_track_file))
+            copy(src_track_file, dst_track_file)
+        
 
 class MainFrame(wx.Frame):
     options = ['Sync all songs and playlists', 'Sync only songs', 'Choose playlist to sync']
 
     def __init__(self, parent, title):
-        wx.Frame.__init__(self, parent, title=title, size=(400, 210))
+        global destination
+
+        wx.Frame.__init__(self, parent, title=title, size=(540, 210))
+        wx.Panel(self, -1, style=wx.TAB_TRAVERSAL|wx.CLIP_CHILDREN|wx.FULL_REPAINT_ON_RESIZE)
 
         filemenu = wx.Menu()
         menuExit = filemenu.Append(wx.ID_EXIT,"E&xit"," Terminate the program")
@@ -18,17 +107,65 @@ class MainFrame(wx.Frame):
         self.SetMenuBar(menuBar)  # Adding the MenuBar to the Frame content.
         self.Bind(wx.EVT_MENU, self.on_close, menuExit)
 
-        wx.Choice(self, -1, pos=(10, 20), size=(380, -1), choices = MainFrame.options)
+        gridbag = wx.GridBagSizer(1, 4)
 
-        wx.Button(self, 1, 'Cancel', (210, 150))
-        wx.Button(self, 2, 'Sync', (300, 150))
+        self.choice = MainFrame.options[0]
+        choice = wx.Choice(self, -1, size=(500, -1), choices=MainFrame.options, pos=(20, 20))
+        self.Bind(wx.EVT_CHOICE, self.set_choice, choice)
+        filebrowse.DirBrowseButton(self, -1, size=(500, -1), pos=(20, 50), changeCallback=self.set_destination)
+
+        cancel = wx.Button(self, 1, 'Cancel', pos=(330, 150))
+        cancel.SetConstraints(
+            anchors.LayoutAnchors(cancel, False, False, True, True)
+        )
+        sync = wx.Button(self, 2, 'Sync', pos=(430, 150))
+        sync.SetConstraints(
+            anchors.LayoutAnchors(sync, False, False, True, True)
+        )
         self.Bind(wx.EVT_BUTTON, self.on_close, id=1)
+        self.Bind(wx.EVT_BUTTON, self.on_sync, id=2)
 
-        self.SetSizeHints(minW=400, minH=210, maxW=400, maxH=210)
+        self.SetSizeHints(minW=540, minH=210, maxW=540, maxH=210)
         self.Show(True)
 
     def on_close(self, event):
         self.Close(True)
+
+    def set_destination(self, evt):
+        global destination
+        destination = evt.GetString()
+
+    def set_choice(self, evt):
+        self.choice = evt.GetString()
+
+    def on_sync(self, event):
+        global destination
+
+        itunes = iTunes()
+        progress = None
+
+        print "Destination is: {0}".format(destination)
+        if not destination:
+            msg = wx.MessageDialog(self, 'You must enter a destination directory first.', "Error", wx.OK|wx.ICON_INFORMATION)
+            msg.ShowModal()
+            msg.Destroy()
+            return
+
+
+        print "Choice is: {0}".format(self.choice)
+        if self.choice == 'Sync all songs and playlists...':
+            # First playlist should be the special 'Music' playlist, which has all tracks
+            total_tracks = len(itunes.playlists()[0].tracks)
+            progress = wx.ProgressDialog('Syncing iTunes Tracks', 'Copying track...', 
+                    maximum=total_tracks, style=0|wx.PD_CAN_ABORT)
+            (synced, skipped) = sync_playlists(itunes.playlists())
+            msg = wx.MessageDialog(self, 'Synced {0} tracks. {1} tracks could not be synced due to DRM'.
+                        format(synced, skipped), "Finished!", wx.OK | wx.ICON_INFORMATION)
+            msg.ShowModal()
+            msg.Destroy()
+
+        if progress:
+            progress.Destroy()
 
 
 app = wx.App(False)
